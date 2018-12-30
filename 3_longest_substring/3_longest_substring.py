99d5e1869771d41ae20ccaa781bb3ef163302d93
Jay <jay@Jays-MacBook-Pro.local>
2018-12-30 18:04:55 +0800

    fine tune problem 3

--- 3_longest_substring/3_longest_substring.py
+++ 3_longest_substring/3_longest_substring.py
@@ -6,31 +6,15 @@ class Solution:
         :type s: str
         :rtype: int
         """
-        end_idx = len(s) - 1
         longest_len = 0
         longest_list = []
-        next_longest_list = []
-        done = False
-        for idx, x in enumerate(s):
-            next_longest_list.append(x)
-            first_match_idx = next_longest_list.index(x)
-            next_longest_len = len(next_longest_list) - 1
+        for x in s:
+            if x in longest_list:
+                reset_start = longest_list.index(x) + 1
+                del longest_list[:reset_start]
 
-            if first_match_idx != next_longest_len or next_longest_len == 0:
-                if next_longest_len >= longest_len:
-                    longest_len = next_longest_len if next_longest_len != 0 else next_longest_len + 1
-                    longest_list = next_longest_list[:longest_len]
-
-                if next_longest_len != 0:
-                    del next_longest_list[:first_match_idx+1]
-                if idx == end_idx:
-                    done = True
-
-        if not done:
-            next_longest_len = len(next_longest_list)
-            if next_longest_len >= longest_len:
-                longest_len = next_longest_len
-                longest_list = next_longest_list
+            longest_list.append(x)
+            longest_len = max(len(longest_list), longest_len)
         return longest_len
 
 s = "ggububgvfk"
